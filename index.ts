6499dd009c7298fff31f2c9f302c108585faf52b
Anton Korneyev <korneevast@altarix.ru>
2022-07-13 11:39:05 +0400

    graphql

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/test", (req, res) => {
-  res.send("Express + TypeScript Server");
-});
+const schema = buildSchema(`
+  type Query {
+    name: String
+  }
+`);
+
+const root = {
+  name: () => {
+    console.log("user name called");
+    ("Anton");
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
